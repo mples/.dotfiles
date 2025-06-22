062fcd8fc7c87207caaaba474c7066d93335f677
mples <mateusz.plesinski@gmail.com>
2025-06-22 23:02:03 +0200

    change to tokyo night theme

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,30 +1,31 @@
 -- Pull in the wezterm API
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config = {
-	default_cursor_style = "SteadyBar",
-	--color_scheme = "Dracula (base16)",
-	color_scheme = "Nord (Gogh)",
-	automatically_reload_config = true,
-	window_close_confirmation = "NeverPrompt",
-	-- window_decorations = "RESIZE",
-	font = wezterm.font("JetBrains Mono", { weight = "Bold" }),
-	font_size = 12.5,
-	check_for_updates = false,
-	use_fancy_tab_bar = false,
-	tab_bar_at_bottom = false,
-	enable_tab_bar = false,
-	window_padding = {
-		left = 3,
-		right = 3,
-		top = 0,
-		bottom = 0,
-	},
+    default_cursor_style = 'SteadyBar',
+    color_scheme = 'Tokyo Night',
+    -- color_scheme = "Dracula (base16)",
+    -- color_scheme = "Nord (Gogh)",
+    automatically_reload_config = true,
+    window_close_confirmation = 'NeverPrompt',
+    -- window_decorations = "RESIZE",
+    font = wezterm.font('JetBrains Mono', { weight = 'Bold' }),
+    font_size = 12.5,
+    check_for_updates = false,
+    use_fancy_tab_bar = false,
+    tab_bar_at_bottom = false,
+    enable_tab_bar = false,
+    window_padding = {
+        left = 3,
+        right = 3,
+        top = 0,
+        bottom = 0,
+    },
 }
-config.default_domain = "WSL:Ubuntu-24.04"
+config.default_domain = 'WSL:Ubuntu-24.04'
 -- and finally, return the configuration to wezterm
 return config
